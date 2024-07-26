e7e5fee67afdc6798dbf6a66575539a4e2acddab
yuxi <2318704295@qq.com>
2024-07-26 08:17:07 -0700

    无初始化

--- tcp_client.c
+++ tcp_client.c
@@ -26,9 +26,16 @@ int main(int argc,char *argv[])
         error_handling("socket() error");
     
     memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-    serv_addr.sin_port=htons(atoi(argv[2]));
+    serv_addr.sin_family=AF_INET;  //地址族 IPV4
+    /*
+    in_addr_t inet_addr(const char *string); 成功时返回32位大端序整数型值，失败时返回INADDR_NONE
+    把字符串形式的IP地址转换成32位整数型数据,在转换类型的同时进行网络字节序转换
+
+    实际编程中若要调用inet_addr函数，需将转换后的IP地址信息代入sockaddr_in结构体中声明的in_addr结构体变量
+    而inet_aron函数则不需要此过程。原因在于若传递in_addr结构体变量地址值，函数会自动把结果填入该结构体变量。
+    */
+    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);  //32位ip地址信息，以网络字节序保存
+    serv_addr.sin_port=htons(atoi(argv[2])); //16位端口号 以网络字节序保存
 
     if(connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1)
         error_handling("connect() error!");
